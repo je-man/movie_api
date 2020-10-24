82c0bedc13583d5ab9cc665106481ae5fff81c7d
je-man <jennmanay@gmail.com>
2020-10-24 16:05:54 -0400

    enable cors for local react app/all domains

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get("/documentation", (req, res) => {
 });
 
 //see all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
